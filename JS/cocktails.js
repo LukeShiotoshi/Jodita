bf63370131b55d7b0fe55bf4274b93a3959d3acc
LukeShioto <lukerobruzzo@gmail.com>
2025-07-16 13:01:33 -0300

    Update cocktails.js

--- JS/cocktails.js
+++ JS/cocktails.js
@@ -1,3 +1,8 @@
+function renderVotingButtons(drinkName) {
+  return `<button onclick="vote('${drinkName}')"
+    class="bg-yellow-300 text-black px-4 py-2 rounded font-bold hover:bg-yellow-400 transition">Bancar</button>`;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const counterDiv = document.getElementById('drink-counter');
   const btnFetch = document.getElementById('cocktailBtn');
